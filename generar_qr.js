5a5634a70de6ff40c79212a583b5aeccc951ae84
Juan <Juan-ma123@hotmail.com>
2026-09-11 15:35:02 -0500

    Add QR code generator script using Node.js

---
+++ generar_qr.js
@@ -0,0 +1,13 @@
+﻿const QRCode = require('qrcode');
+const contratoId = 'CAJ7ONIUSPKD2XVPTVVWIMZVYYSBKFCHYZGNKO4MCQI5ACUOZHBRVWHF';
+const idLote = 'LOTE-2026-001';
+const urlPasaporte = 'https://stellar.expert/explorer/testnet/contract/' + contratoId;
+const archivoSalida = 'qr_' + idLote + '.png';
+
+QRCode.toFile(archivoSalida, urlPasaporte, {
+  color: { dark: '#000000', light: '#FFFFFF' },
+  width: 400
+}, function (err) {
+  if (err) throw err;
+  console.log('✅ Código QR generado exitosamente: ' + archivoSalida);
+});
